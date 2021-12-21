158e5534a77c01438bf83b2944f87340cbcf5a5b
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-12-21 15:55:56 +0700

    Update sql_queries.py

--- project-3-data-warehouse/sql_queries.py
+++ project-3-data-warehouse/sql_queries.py
@@ -7,6 +7,7 @@ config.read('dwh.cfg')
 DWH_ROLE_ARN = config.get('IAM_ROLE','ARN')
 S3_LOG_DATA = config.get('S3','LOG_DATA')
 S3_SONG_DATA = config.get('S3','SONG_DATA')
+S3_LOG_JSONPATH = config.get('S3', 'LOG_JSONPATH')
 
 # DROP TABLES
 
@@ -36,7 +37,10 @@ staging_events_table_create= ("""
     registration numeric, 
     sessionId int, 
     song varchar, 
-    status varchar
+    status varchar,
+    ts bigint, 
+    userAgent varchar, 
+    userId int
     )
 """)
 
@@ -136,9 +140,9 @@ time_table_create = ("""
 staging_events_copy = ("""
     copy staging_events from {}
     credentials 'aws_iam_role={}'
-    format as json 'auto'
-    region 'us-west-2';
-""").format(S3_LOG_DATA, DWH_ROLE_ARN)
+    region 'us-west-2'
+    format json as {};
+""").format(S3_LOG_DATA, DWH_ROLE_ARN, S3_LOG_JSONPATH)
 
 staging_songs_copy = ("""
     copy staging_songs from {}
@@ -150,18 +154,71 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
+    TRUNCATE TABLE songplays;
+    
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    SELECT timestamp 'epoch' + cast(staging_events.ts AS bigint)/1000 * interval '1 second' AS epoch_to_timestamp,
+    staging_events.userid,
+    staging_events.level,
+    songs.song_id,
+    artists.artist_id,
+    staging_events.sessionId,
+    artists.location,
+    staging_events.userAgent
+    FROM staging_events
+    LEFT JOIN songs ON staging_events.song = songs.title
+        AND staging_events.length = songs.duration
+    LEFT JOIN artists ON staging_events.artist = artists.name
+    WHERE staging_events.page = 'NextSong'
+    ;
 """)
 
 user_table_insert = ("""
+    TRUNCATE TABLE users;
+    
+    INSERT INTO users (user_id, first_name, last_name, gender, level)
+    SELECT userId, firstName, lastName, gender, level
+    FROM staging_events
+    WHERE page = 'NextSong'
+    ;
 """)
 
 song_table_insert = ("""
+    TRUNCATE TABLE songs;
+    
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    SELECT song_id, title, artist_id, year, duration 
+    FROM staging_songs
+    ;
 """)
 
 artist_table_insert = ("""
+    TRUNCATE TABLE artists;
+    
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+    FROM staging_songs
+    ;
 """)
 
 time_table_insert = ("""
+    TRUNCATE TABLE time;
+
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    WITH tmp AS (
+        SELECT timestamp 'epoch' + cast(ts AS bigint)/1000 * interval '1 second' AS epoch_to_timestamp 
+        FROM staging_events
+        WHERE page = 'NextSong'
+        ) 
+    SELECT epoch_to_timestamp AS start_time, 
+    EXTRACT(hour from epoch_to_timestamp) AS hour, 
+    EXTRACT(day from epoch_to_timestamp) AS day, 
+    EXTRACT(week from epoch_to_timestamp) AS week,
+    EXTRACT(month from epoch_to_timestamp) AS month,
+    EXTRACT(year from epoch_to_timestamp) AS year,
+    EXTRACT(weekday from epoch_to_timestamp) AS weekday
+    FROM tmp
+    ;
 """)
 
 # QUERY LISTS
